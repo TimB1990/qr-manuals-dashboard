411dd71aff429c6c8f18859d96c9b48ae1fabf08
TimB1990 <t.boons@live.nl>
2020-03-16 23:02:04 +0100

    sheet design completed partly

--- resources/assets/js/views/main/components/MainQrConfigPanel.vue
+++ resources/assets/js/views/main/components/MainQrConfigPanel.vue
@@ -4,21 +4,25 @@
     <em>toggle one or more products to generate QR-codes on sheet</em>
 
     <table class="table">
-        <tr v-for="selected in selectedProducts" :key="selected.id">
-            <td v-text="selected.artnr"></td>
-            <td>options</td>
-        </tr>
+      <tr v-for="selected in selectedProducts" :key="selected.id">
+        <td v-text="selected.artnr"></td>
+        <td>options</td>
+      </tr>
     </table>
 
+    <!-- this element represents the background like blueprint -->
+    <div class="sticker-sheet-underlay">
+      <div v-for="n in 16" :key="n" class="qr-item-blueprint"></div>
+    </div>
+
     <div class="sticker-sheet">
-      <div class="sticker-sheet-margin-box">
-        <main-qr-item
-          v-for="selected in selectedProducts"
-          :key="selected.id"
-          :artnr="selected.artnr"
-          :kind="selected.kind"
-        ></main-qr-item>
-      </div>
+      <span>paper format: A4</span>
+      <main-qr-item
+        v-for="selected in selectedProducts"
+        :key="selected.id"
+        :artnr="selected.artnr"
+        :kind="selected.kind"
+      ></main-qr-item>
     </div>
   </div>
 </template>
@@ -38,18 +42,43 @@ export default {
 
 <style>
 .sticker-sheet {
-  border: 1px solid blue;
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  border: 1px solid grey;
+  width: 100%;
+  height: 100%;
   width: 210mm;
   height: 297mm;
-  padding: 12.5mm;
+  padding: 11.5mm;
 }
 
-.sticker-sheet-margin-box {
+.sticker-sheet > span {
+    position:absolute;
+  margin-top: -58px;
+  margin-left: 2px;
+  padding-left: 9px;
+  padding-right:9px;
+  background: white;
+}
+
+.sticker-sheet-underlay {
   display: flex;
   flex-wrap: wrap;
   align-content: flex-start;
-  border: 1px solid gray;
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  width: 210mm;
+  height: 297mm;
+  padding: 11.5mm;
+  z-index: -1;
+}
+
+.qr-item-blueprint {
+  height: 55mm;
+  width: 40mm;
+  border: 1px dashed hsl(0, 0%, 65%);
+  border-radius: 12px;
+  margin: 12px;
+  z-index: -1;
 }
 </style>
--- resources/assets/js/views/main/components/MainQrItem.vue
+++ resources/assets/js/views/main/components/MainQrItem.vue
@@ -34,8 +34,8 @@ export default {
 
 <style>
 .qr-item {
-  display: inline-block;
   height: 55mm;
+  width: 40mm;
   border: 1px solid black;
   border-radius: 12px;
   padding: 8px;
@@ -49,14 +49,14 @@ export default {
 
 .qr-item-box {
   padding: 12px;
-  background: linear-gradient(to right, black 3px, transparent 3px) 0 0,
+  /*background: linear-gradient(to right, black 3px, transparent 3px) 0 0,
     linear-gradient(to right, black 3px, transparent 3px) 0 100%,
     linear-gradient(to left, black 3px, transparent 3px) 100% 0,
     linear-gradient(to left, black 3px, transparent 3px) 100% 100%,
     linear-gradient(to bottom, black 3px, transparent 3px) 0 0,
     linear-gradient(to bottom, black 3px, transparent 3px) 100% 0,
     linear-gradient(to top, black 3px, transparent 3px) 0 100%,
-    linear-gradient(to top, black 3px, transparent 3px) 100% 100%;
+    linear-gradient(to top, black 3px, transparent 3px) 100% 100%;*/
 
   background-repeat: no-repeat;
   background-size: 18px 18px;

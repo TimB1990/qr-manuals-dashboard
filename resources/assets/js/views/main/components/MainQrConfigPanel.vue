c81a50d1a75e48e246ad3eaf3733a3df05bb4af7
TimB1990 <t.boons@live.nl>
2020-03-17 19:46:00 +0100

    new paginated selected structure yet to implement

--- resources/assets/js/views/main/components/MainQrConfigPanel.vue
+++ resources/assets/js/views/main/components/MainQrConfigPanel.vue
@@ -2,13 +2,12 @@
   <div>
     <strong>QR configuration panel</strong>
     <em>toggle one or more products to generate QR-codes on sheet</em>
+    <button>next</button><button>previous</button>
 
-    <table class="table">
-      <tr v-for="selected in selectedProducts" :key="selected.id">
-        <td v-text="selected.artnr"></td>
-        <td>options</td>
-      </tr>
-    </table>
+    <p>
+      <code>{{ selectedProducts }}</code>
+    </p>
+    <br />
 
     <!-- this element represents the background like blueprint -->
     <div class="sticker-sheet-underlay">
@@ -16,7 +15,7 @@
     </div>
 
     <div class="sticker-sheet">
-      <span>paper format: A4</span>
+      <span>paper format: A4, page: 1</span>
       <main-qr-item
         v-for="selected in selectedProducts"
         :key="selected.id"
@@ -32,9 +31,51 @@ import MainQrItem from "./MainQrItem";
 export default {
   name: "mainQrConfigPanel",
   components: { MainQrItem },
+
+  data() {
+    return {
+      page_size: 16,
+      current_page: 1
+    };
+  },
+
+  methods: {
+    nextPage() {
+      const max_pages = Math.ceil(selectedProducts.length / this.page_size);
+      if (this.current_page < max_pages) {
+        this.current_page += 1;
+      } else {
+        this.current_page = max_pages;
+      }
+    },
+    prevPage() {
+      if (this.current_page > 1) {
+        this.current_page -= 1;
+      } else {
+        this.current_page = 1;
+      }
+    }
+  },
+
   computed: {
     selectedProducts() {
       return this.$store.state.selectedProducts;
+    },
+
+    paginated() {
+      const page_size = this.page_size;
+      let items = this.$store.state.selectedProducts;
+      let pages = Math.ceil(items.length / page_size);
+      let paginated = [];
+
+      for (var i = 0; i < pages; i++) {
+        paginated.push({
+          page: i + 1,
+          items: items.slice((i + 1 - 1) * page_size, (i + 1) * page_size)
+        });
+      }
+
+      return paginated;
     }
   }
 };
@@ -54,15 +95,17 @@ export default {
 }
 
 .sticker-sheet > span {
-    position:absolute;
+  position: absolute;
   margin-top: -58px;
   margin-left: 2px;
   padding-left: 9px;
-  padding-right:9px;
+  padding-right: 9px;
   background: white;
 }
 
 .sticker-sheet-underlay {
+  background-color: hsl(300, 50%, 90%);
+  opacity: 0.5;
   display: flex;
   flex-wrap: wrap;
   align-content: flex-start;
